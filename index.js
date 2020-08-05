7d7ffd66484ea9a644d05f61012e10ce4d96d6ee
Athar Laique <atharlaique15@gmail.com>
2020-08-05 12:30:10 +0500

    express basic boilerplate

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express=require('express');
 const app=express();
+const morgan=require('morgan');
+const cors=require('cors');
+const bodyParser=require('body-parser');
 require('dotenv').config()
+//Envoirement variables
+const port =process.env.PORT || 3001;
+const env=process.env.NODE_ENV
 
+//App Middleware
+app.use(morgan('dev'))
+if(env==='development'){
+        app.use(cors({origin:'http://localhost:3000'}))
+    }
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+//Routes 
+app.get('/',(req,res)=>{
+    res.send({
+     msg:'Welcome'
+    })
+})
 
-const port =process.env.PORT || 3001
 app.listen(port,()=>{
  console.log(`App is running on :${port}`)
 })
